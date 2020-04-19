7a3bcbcb8d10a1ef33d9853c1545e8065810ce06
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-19 20:48:51 +0200

    Fetching store data and displaying

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,41 @@ import { Store } from './Store/store';
 import './App.css';
 
 function App(): JSX.Element {
-  const store = React.useContext(Store);
+  const { state, dispatch } = React.useContext(Store);
 
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
+  const fetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes';
+    const data = await fetch(URL);
+    const dataJson = await data.json();
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJson._embedded.episodes,
+    });
+  };
+  console.log(state);
   return (
     <React.Fragment>
-      {console.log(store)}
-      <h1>Rick & Morty</h1>
-      <p>Pick Your Favourite Character</p>
+      <header className='header'>
+        <h1>Rick & Morty</h1>
+        <p>Pick Your Favourite Character</p>
+      </header>
+      <section className='episode-layout'>
+        {state.episodes.slice(0, 20).map((epi: any) => {
+          return (
+            <section key={epi.id} className='episode-name'>
+              <img src={epi.image.medium} alt={epi.name} />
+              <div>{epi.name}</div>
+              <section>
+                Season:{epi.season} Number:{epi.number}
+              </section>
+            </section>
+          );
+        })}
+      </section>
     </React.Fragment>
   );
 }
--- src/Store/store.tsx
+++ src/Store/store.tsx
@@ -9,11 +9,12 @@ interface IAction {
   payload: any;
 }
 const initialState: IState = { episodes: [], favourites: [] };
-export const Store = React.createContext<IState>(initialState);
+
+export const Store = React.createContext<IState | any>(initialState);
 
 function reducer(state: IState, action: IAction): IState {
   switch (action.type) {
-    case 'FETCH-DATA':
+    case 'FETCH_DATA':
       return { ...state, episodes: action.payload };
     default:
       return state;
@@ -21,5 +22,10 @@ function reducer(state: IState, action: IAction): IState {
 }
 
 export function StoreProvider(props: any): JSX.Element {
-  return <Store.Provider value={initialState}>{props.children}</Store.Provider>;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      {props.children}
+    </Store.Provider>
+  );
 }
