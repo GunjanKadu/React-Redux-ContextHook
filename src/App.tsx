662562e419ce11705f8343e862e5f7033a6bfd8e
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-19 20:14:08 +0200

    Create Store and context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Store } from './Store/store';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const store = React.useContext(Store);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {console.log(store)}
+      <h1>Rick & Morty</h1>
+      <p>Pick Your Favourite Character</p>
+    </React.Fragment>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { StoreProvider, Store } from './Store/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <StoreProvider>
     <App />
-  </React.StrictMode>,
+  </StoreProvider>,
   document.getElementById('root')
 );
 
